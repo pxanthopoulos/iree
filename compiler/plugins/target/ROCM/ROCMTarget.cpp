5f810daff38e496bc7e5ac85c4b9b1951b742801
Harsh Menon <harsh@nod-labs.com>
2024-02-14 18:03:07 -0800

    Turn off SLP vectorizer

--- compiler/plugins/target/ROCM/ROCMTarget.cpp
+++ compiler/plugins/target/ROCM/ROCMTarget.cpp
@@ -153,7 +153,7 @@ public:
     fam.registerPass([&] { return targetMachine.getTargetIRAnalysis(); });
 
     llvm::PipelineTuningOptions pto;
-    pto.SLPVectorization = true;
+    pto.SLPVectorization = false;
 
     llvm::PassInstrumentationCallbacks pic;
 
